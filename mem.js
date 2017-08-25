3bcdaf6ad2d5e56232f293b2e29b78e4bf31a81b
Robert Scarlett <scarlettt10@msn.com>
2017-08-25 11:59:54 -0600

    added a shuffle method to mem.js

--- mem.js
+++ mem.js
@@ -1,14 +1,15 @@
 
-var r_1 = ["&", "*", "!"]
-var r_2 = ["&", "*", "!"]
-var r_3 = ["&", "*", "!"]
-var r_4 = ["&", "*", "!"]
+var r_1 = ["*", "*", "&", "&", "!", "!", "@", "@", "$", "$", "%", "%"]
 
-//need random assign symbol to html block
+
+//need random assign symbol to html "td"
 //need be able to click on block reveal square
 
+$(document).ready(function(shuffleArray) {
 
-$(document).ready (function(){
-
+function shuffleArray(r_1) {
+  for (var j, x, i = r_1.length; i; j = parseInt(Math.random() * i), x = r_1[--i], r_1[i] = r_1[j], r_1[j] = x);
+  return ("td");
+}
 
 })
