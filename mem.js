3460b6e35d4b8654bba7c56d767751b3a5a471ef
Robert Scarlett <scarlettt10@msn.com>
2017-08-25 10:46:46 -0600

    created grid, added styles

---
+++ mem.js
@@ -0,0 +1,14 @@
+
+var r_1 = ["&", "*", "!"]
+var r_2 = ["&", "*", "!"]
+var r_3 = ["&", "*", "!"]
+var r_4 = ["&", "*", "!"]
+
+//need random assign symbol to html block
+//need be able to click on block reveal square
+
+
+$(document).ready (function(){
+
+
+})
